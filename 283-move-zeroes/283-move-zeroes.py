b53d0ac1d027f9f2628458f0652975b83ebb0a51
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2022-10-07 01:37:27 +0300

    Time: 507 ms (20.12%), Space: 15.7 MB (16.65%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,27 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if (len(nums) == 1) or (len(nums) == 0):
+            return
+        
+        if (len(nums) == 2) and (nums[0] == 0):
+            nums[0], nums[1] = nums[1], nums[0]
+            return
+        
+        first_pointer = 0
+        second_pointer = 1
+        while(second_pointer < len(nums)-1):
+            if nums[first_pointer] == 0:
+                while (nums[second_pointer] == 0) and (second_pointer < len(nums)-1):
+                    second_pointer += 1
+                nums[first_pointer], nums[second_pointer] = nums[second_pointer], nums[first_pointer]
+
+            if (second_pointer - first_pointer == 1) and (second_pointer < len(nums)-1): 
+                second_pointer += 1  
+
+            first_pointer += 1
+            
+        if nums[first_pointer] == 0:
+             nums[first_pointer], nums[second_pointer] = nums[second_pointer], nums[first_pointer]

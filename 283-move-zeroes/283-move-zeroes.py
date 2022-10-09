cdca665c63b5924d408dac9ae02618a331b8f9e1
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2022-10-09 11:44:49 +0300

    Time: 239 ms (75.95%), Space: 15.6 MB (64.39%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,22 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if (len(nums) == 1) or (len(nums) == 0):
-            return
-
         first_pointer = 0
-        second_pointer = 1
+        second_pointer = 0
         
-        while(second_pointer < len(nums)-1):
+        while (second_pointer < len(nums) -1):
             if nums[first_pointer] == 0:
-                while (nums[second_pointer] == 0) and (second_pointer < len(nums)-1):
-                    second_pointer += 1
+                while (nums[second_pointer] == 0) & (second_pointer < len(nums) -1):
+                    second_pointer +=1
                 nums[first_pointer], nums[second_pointer] = nums[second_pointer], nums[first_pointer]
-
-            if (second_pointer - first_pointer == 1) and (second_pointer < len(nums)-1): 
-                second_pointer += 1  
-
+                
+            if second_pointer - first_pointer <= 1:
+                second_pointer += 1
+                
             first_pointer += 1
             
         if nums[first_pointer] == 0:
-             nums[first_pointer], nums[second_pointer] = nums[second_pointer], nums[first_pointer]
+            nums[first_pointer], nums[len(nums)-1] = nums[len(nums)-1], nums[first_pointer]

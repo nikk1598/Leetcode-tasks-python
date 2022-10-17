7e6ff4dcd8b39192e392aeb4d4cf71950f07f5fd
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2022-10-18 00:13:10 +0300

    Time: 68 ms (21.66%), Space: 13.9 MB (58.94%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        pointer = len(digits) - 1
+        
+        if digits[pointer] < 9:
+            digits[pointer] += 1
+            return digits
+        
+        while (digits[pointer] == 9) & (pointer > 0):
+            pointer -= 1
+            
+        if (digits[pointer] == 9) & (pointer == 0):
+            return [1] + [0]*len(digits)
+        elif digits[pointer] != 9:
+            digits[pointer] += 1
+            while pointer < len(digits) - 1:
+                pointer += 1
+                digits[pointer] = 0
+            return digits
+             
+        
+            
+            

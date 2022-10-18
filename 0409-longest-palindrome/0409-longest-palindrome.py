f5a61d1c0c6c197f3ca523cefe576662b6767cf9
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2022-10-19 01:39:07 +0300

    Time: 59 ms (53.72%), Space: 13.9 MB (22.82%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -11,9 +11,7 @@ class Solution:
         left_right_count = 0
         
         for x in dic.keys():
-            if dic[x] == 1:
-                middle_count += 1
-            elif dic[x] % 2 == 0:
+            if dic[x] % 2 == 0:
                 left_right_count += dic[x]
             elif (dic[x] % 2) != 0:
                 left_right_count += dic[x]-1

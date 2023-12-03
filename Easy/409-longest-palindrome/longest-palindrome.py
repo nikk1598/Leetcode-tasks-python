c8d6c7611c1833cfa243a1ae3f8ed8c5766474ba
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-12-03 06:21:48 +0300

    Time: 46 ms (20.12%) | Memory: 16.2 MB (53.68%) - LeetSync

---
+++ Easy/409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        dic = {}
+        for x in s:
+            if x in dic:
+                dic[x] += 1
+            else:
+                dic[x] = 1
+                
+        middle_count = 0
+        left_right_count = 0
+        
+        for x in dic.keys():
+            if dic[x] % 2 == 0:
+                left_right_count += dic[x]
+            elif (dic[x] % 2) != 0:
+                left_right_count += dic[x]-1
+                middle_count += 1
+
+        if middle_count == 0:
+            return left_right_count
+        else:
+            return left_right_count+1

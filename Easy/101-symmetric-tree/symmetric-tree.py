ace19e2092485a3413c69a9b1394868609df8920
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2023-12-03 06:20:26 +0300

    Time: 45 ms (28.82%) | Memory: 16.6 MB (22.46%) - LeetSync

---
+++ Easy/101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        left_subtree = []
+        def get_left_subtree(root):
+            if root:
+                left_subtree.append(get_left_subtree(root.left))
+                left_subtree.append(root.val)   
+                left_subtree.append(get_left_subtree(root.right))
+            else: 
+                return
+        get_left_subtree(root.left)
+        
+        right_subtree = []
+        def get_right_subtree(root):
+            if root:
+                right_subtree.append(get_right_subtree(root.right))
+                right_subtree.append(root.val)   
+                right_subtree.append(get_right_subtree(root.left))
+            else: 
+                return
+        get_right_subtree(root.right)
+        
+        if len(left_subtree) != len(right_subtree):
+            return False
+        
+        for x,y in zip(left_subtree, right_subtree):
+            if x != y:
+                return False
+        return True
+        

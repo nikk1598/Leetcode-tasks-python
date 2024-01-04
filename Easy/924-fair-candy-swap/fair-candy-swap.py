c406b2a33e59d5462d193049833aff61ce340b5e
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-01-04 22:47:32 +0300

    Time: 6850 ms (5.06%) | Memory: 20 MB (8.76%) - LeetSync

--- Easy/924-fair-candy-swap/fair-candy-swap.py
+++ Easy/924-fair-candy-swap/fair-candy-swap.py
@@ -1,11 +1,10 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        aliceSizesSet = set(aliceSizes)
         for num in bobSizes:
             ## bobSum - num + x = aliceSum - x + num
             ## 2x = aliceSum - bobSum + 2*num
             ## x = (aliceSum - bobSum + 2*num) / 2
             x = (sum(aliceSizes) - sum(bobSizes) + 2*num) // 2
-            if x in aliceSizesSet:
+            if x in set(aliceSizes):
                 return [x, num]
 

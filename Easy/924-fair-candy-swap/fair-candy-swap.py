bead9f967c62634dcc5cf8bccf4aa735c0a993c7
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-01-04 22:56:02 +0300

    Time: 288 ms (95.21%) | Memory: 20.1 MB (8.76%) - LeetSync

--- Easy/924-fair-candy-swap/fair-candy-swap.py
+++ Easy/924-fair-candy-swap/fair-candy-swap.py
@@ -1,13 +1,20 @@
 class Solution:
     def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        aliceSum = sum(aliceSizes)
+        bobSum = sum(bobSizes)
         aliceSizesSet = set(aliceSizes)
-        
+
         for num in bobSizes:
-            x = (sum(aliceSizes) - sum(bobSizes) + 2*num) // 2
+            """
+            The element, x, that we are looking for in AliceSizesSet
+            should equal the sums of AliceSum and BobSum when replacing x --> num in AliceSizes
+            and replacing  num --> x in BobSizes.
+        
+            bobSum - num + x = aliceSum - x + num
+            ## 2x = aliceSum - bobSum + 2*num
+            ## x = (aliceSum - bobSum + 2*num) / 2
+            """
+            x = (aliceSum - bobSum + 2*num) // 2
             if x in aliceSizesSet:
                 return [x, num]
-
-    ## bobSum - num + x = aliceSum - x + num
-    ## 2x = aliceSum - bobSum + 2*num
-    ## x = (aliceSum - bobSum + 2*num) / 2
 

8d59101a6dc0b71f02a70e9dacf0e66f94847e0f
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-05 00:27:52 +0300

    Time: 65 ms (55.41%) | Memory: 16.6 MB (84.72%) - LeetSync

---
+++ Easy/567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        d = {}
+        for char in s1:
+            if char in d:
+                d[char] += 1
+            else:
+                d[char] = 1
+
+        i = 0
+        j = -1
+        zero_count = 0
+        while j < len(s2)-1:
+            j += 1
+            if s2[j] in d:
+                d[s2[j]] -= 1
+                if d[s2[j]] == 0:
+                    zero_count += 1
+                if d[s2[j]] == -1:
+                    zero_count -= 1
+        
+            if j-i == len(s1):
+                i += 1
+                if s2[i-1] in d:
+                    d[s2[i-1]] += 1
+                    if d[s2[i-1]] == 1:
+                        zero_count -= 1
+                    if d[s2[i-1]] == 0:
+                        zero_count += 1
+
+            if zero_count == len(d):
+                return True
+
+        return False
+

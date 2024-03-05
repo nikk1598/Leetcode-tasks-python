fa97d625d5c7ae89048d8f83f1462cf28696f737
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-05 18:57:17 +0300

    Time: 39 ms (61.78%) | Memory: 16.6 MB (84.59%) - LeetSync

--- Easy/88-merge-sorted-array/merge-sorted-array.py
+++ Easy/88-merge-sorted-array/merge-sorted-array.py
@@ -3,19 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if m==0:
-            for i in range(len(nums2)):
+        
+        if n == 0:
+            return
+
+        if m == 0:
+            for i in range(n):
                 nums1[i] = nums2[i]
-            return nums1
-            
-        for x in nums2:
-            nums1[m] = x
-            m += 1
-            for i in range(m-1, 1, -1):
-                if nums1[i] < nums1[i-1]:
-                    nums1[i], nums1[i-1] = nums1[i-1], nums1[i]
-                else:
-                    break
-                    
-            if nums1[1] < nums1[0]:
-                 nums1[1], nums1[0] = nums1[0], nums1[1]
+            return
+
+
+        for i in range(len(nums1)-n-1, -1, -1):
+            nums1[i+n] = nums1[i]
+     
+        i = n
+        j = 0
+        curr_pos = 0
+        while True:
+            if i >= len(nums1):
+                while j < len(nums2):
+                    nums1[curr_pos] = nums2[j]
+                    curr_pos += 1
+                    j += 1
+                break
+            if j >= len(nums2):
+                break
+            if nums1[i] < nums2[j]:
+                nums1[curr_pos] = nums1[i]
+                i += 1
+            else:
+                nums1[curr_pos] = nums2[j]
+                j += 1
+            curr_pos += 1

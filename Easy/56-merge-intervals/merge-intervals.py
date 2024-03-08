fc859aa04d07e9c8e0a175072c1b2f462ec7a070
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-08 21:15:15 +0300

    Time: 142 ms (5.26%) | Memory: 21.5 MB (11.05%) - LeetSync

--- Easy/56-merge-intervals/merge-intervals.py
+++ Easy/56-merge-intervals/merge-intervals.py
@@ -2,21 +2,22 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         events = []
         for interval in intervals:
-            events.append((interval[0], 0))
-            events.append((interval[1], 1))
+            events.append([interval[0], 0])
+            events.append([interval[1], 1])
         
         events.sort()
+        start_pos = 0
         count = 0
-        i = 0
         ans = []
+
         for event in events:
             if event[1] == 0:
                 count += 1
                 if count == 1:
-                    i = event[0]
+                    start_pos = event[0]
             else:
                 count -= 1
                 if count == 0:
-                    ans.append([i, event[0]])
+                    ans.append([start_pos, event[0]])
 
         return ans

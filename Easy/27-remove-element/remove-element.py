334b67d05977e45e36101559cb55b8205d0d7fdd
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-08 16:16:41 +0300

    Time: 43 ms (18.35%) | Memory: 16.6 MB (60.22%) - LeetSync

--- Easy/27-remove-element/remove-element.py
+++ Easy/27-remove-element/remove-element.py
@@ -1,26 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        first_pointer = 0
-        second_pointer = 0
-        
-        if len(nums) == 0:
-            return 0
-        
-        while(second_pointer < len(nums)):
-            if nums[first_pointer] == val:
-                while(nums[second_pointer] == val) & (second_pointer < len(nums)-1):
-                    second_pointer += 1
-                nums[second_pointer], nums[first_pointer] = nums[first_pointer], nums[second_pointer]
-            
-            if second_pointer - first_pointer == 0:
-                second_pointer += 1
-                
-            first_pointer += 1
-            
-        i = len(nums) - 1
-        
-        while (nums[i] == val) & (i > -1):
-            i -= 1
-
-        nums = nums[0:i+1]
-        return i+1
+        k = len(nums)
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != val:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            else:
+                k -= 1
+        return k

b6f989deedb3d2edc4175dcac813e2ce5c1f8cb6
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-10 00:23:23 +0300

    Time: 69 ms (25.38%) | Memory: 16.7 MB (67.79%) - LeetSync

--- Easy/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ Easy/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,27 +2,26 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) == 0:
             return 0
+        d = {s[0]: 1}
         i = 0
         j = 0
         count = 1
-        ans = 0
-        d = {s[0]:1}
+        ans = 1
 
         while True:
-            if count == j - i + 1:
+            if count == j-i+1:
                 ans = max(ans, j-i+1)
                 j += 1
-                if j >= len(s):
+                if j == len(s):
                     break
-                if s[j] not in d:
-                    d[s[j]] = 1
-                else:
+                if s[j] in d:
                     d[s[j]] += 1
+                else:
+                    d[s[j]] = 1
                 if d[s[j]] == 1:
                     count += 1
                 elif d[s[j]] == 2:
                     count -= 1
-                
             else:
                 i += 1
                 d[s[i-1]] -= 1
@@ -30,8 +29,11 @@ class Solution:
                     count += 1
                 elif d[s[i-1]] == 0:
                     count -= 1
-            print(i, j, ans)
-
+            
         return ans
             
- 
+
+
+
+
+

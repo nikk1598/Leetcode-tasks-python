4937ba8221b4932c09ba9ef502ec882cb2cdb270
nikk1598 <40841547+nikk1598@users.noreply.github.com>
2024-03-10 01:13:39 +0300

    Time: 41 ms (49.63%) | Memory: 16.6 MB (38.33%) - LeetSync

--- Easy/88-merge-sorted-array/merge-sorted-array.py
+++ Easy/88-merge-sorted-array/merge-sorted-array.py
@@ -1,33 +1,20 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
+        if len(nums2) == 0:
+            return nums1
         
-        if n == 0:
-            return
-
-        if m == 0:
-            for i in range(n):
+        if len(nums1) == len(nums2):
+            for i in range(len(nums1)):
                 nums1[i] = nums2[i]
-            return
-
-
-        for i in range(len(nums1)-n-1, -1, -1):
-            nums1[i+n] = nums1[i]
-     
+    
+        for i in range(m-1, -1, -1):
+            nums1[i], nums1[i+len(nums2)] = nums1[i+len(nums2)], nums1[i]
+        
         i = n
         j = 0
         curr_pos = 0
-        while True:
-            if i >= len(nums1):
-                while j < len(nums2):
-                    nums1[curr_pos] = nums2[j]
-                    curr_pos += 1
-                    j += 1
-                break
-            if j >= len(nums2):
-                break
+
+        while (j < len(nums2)) and (i < len(nums1)):
             if nums1[i] < nums2[j]:
                 nums1[curr_pos] = nums1[i]
                 i += 1
@@ -35,3 +22,16 @@ class Solution:
                 nums1[curr_pos] = nums2[j]
                 j += 1
             curr_pos += 1
+        
+        while (j < len(nums2)):
+            nums1[curr_pos] = nums2[j]
+            j += 1
+            curr_pos += 1
+
+        return nums1
+
+
+
+
+           
+  
